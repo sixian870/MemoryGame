573d8263059db30e12ee9b3d49177689fdbce02b
sixian870 <alice0701lee@gmail.com>
2026-05-25 04:19:45 +0800

    加入專案檔案。

---
+++ MemoryGame/frmMemoryGame.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace MemoryGame
+{
+    partial class frmMemoryGame
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pnlTop = new System.Windows.Forms.Panel();
+            this.pnlButtom = new System.Windows.Forms.Panel();
+            this.btnLv1 = new System.Windows.Forms.Button();
+            this.btnLv2 = new System.Windows.Forms.Button();
+            this.btnLv3 = new System.Windows.Forms.Button();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.timeBar = new System.Windows.Forms.ProgressBar();
+            this.lblRemaining = new System.Windows.Forms.Label();
+            this.btnRestart = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.gamePanel = new System.Windows.Forms.TableLayoutPanel();
+            this.lbls = new System.Windows.Forms.Label();
+            this.pnlTop.SuspendLayout();
+            this.pnlButtom.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlTop
+            // 
+            this.pnlTop.Controls.Add(this.lbls);
+            this.pnlTop.Controls.Add(this.lblRemaining);
+            this.pnlTop.Controls.Add(this.timeBar);
+            this.pnlTop.Controls.Add(this.lblTime);
+            this.pnlTop.Controls.Add(this.btnLv3);
+            this.pnlTop.Controls.Add(this.btnLv2);
+            this.pnlTop.Controls.Add(this.btnLv1);
+            this.pnlTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlTop.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.pnlTop.Location = new System.Drawing.Point(0, 0);
+            this.pnlTop.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pnlTop.Name = "pnlTop";
+            this.pnlTop.Size = new System.Drawing.Size(543, 66);
+            this.pnlTop.TabIndex = 0;
+            // 
+            // pnlButtom
+            // 
+            this.pnlButtom.Controls.Add(this.btnRestart);
+            this.pnlButtom.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pnlButtom.Location = new System.Drawing.Point(0, 574);
+            this.pnlButtom.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pnlButtom.Name = "pnlButtom";
+            this.pnlButtom.Size = new System.Drawing.Size(543, 39);
+            this.pnlButtom.TabIndex = 2;
+            // 
+            // btnLv1
+            // 
+            this.btnLv1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnLv1.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.btnLv1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnLv1.Location = new System.Drawing.Point(100, 11);
+            this.btnLv1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnLv1.Name = "btnLv1";
+            this.btnLv1.Size = new System.Drawing.Size(100, 31);
+            this.btnLv1.TabIndex = 0;
+            this.btnLv1.Text = "🚩第一關 4x4";
+            this.btnLv1.UseVisualStyleBackColor = false;
+            // 
+            // btnLv2
+            // 
+            this.btnLv2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnLv2.BackColor = System.Drawing.SystemColors.Control;
+            this.btnLv2.Location = new System.Drawing.Point(208, 11);
+            this.btnLv2.Margin = new System.Windows.Forms.Padding(4);
+            this.btnLv2.Name = "btnLv2";
+            this.btnLv2.Size = new System.Drawing.Size(100, 31);
+            this.btnLv2.TabIndex = 1;
+            this.btnLv2.Text = "🚩第二關 6x6";
+            this.btnLv2.UseVisualStyleBackColor = false;
+            // 
+            // btnLv3
+            // 
+            this.btnLv3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnLv3.BackColor = System.Drawing.SystemColors.Control;
+            this.btnLv3.Location = new System.Drawing.Point(316, 11);
+            this.btnLv3.Margin = new System.Windows.Forms.Padding(4);
+            this.btnLv3.Name = "btnLv3";
+            this.btnLv3.Size = new System.Drawing.Size(100, 31);
+            this.btnLv3.TabIndex = 2;
+            this.btnLv3.Text = "🚩第三關 8x8";
+            this.btnLv3.UseVisualStyleBackColor = false;
+            // 
+            // lblTime
+            // 
+            this.lblTime.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblTime.AutoSize = true;
+            this.lblTime.Location = new System.Drawing.Point(13, 46);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(81, 16);
+            this.lblTime.TabIndex = 3;
+            this.lblTime.Text = "⏰剩餘時間 : ";
+            // 
+            // timeBar
+            // 
+            this.timeBar.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.timeBar.ForeColor = System.Drawing.Color.PaleGoldenrod;
+            this.timeBar.Location = new System.Drawing.Point(98, 49);
+            this.timeBar.Name = "timeBar";
+            this.timeBar.Size = new System.Drawing.Size(369, 10);
+            this.timeBar.TabIndex = 4;
+            // 
+            // lblRemaining
+            // 
+            this.lblRemaining.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblRemaining.AutoSize = true;
+            this.lblRemaining.Location = new System.Drawing.Point(423, 20);
+            this.lblRemaining.Name = "lblRemaining";
+            this.lblRemaining.Size = new System.Drawing.Size(98, 16);
+            this.lblRemaining.TabIndex = 5;
+            this.lblRemaining.Text = "剩餘未配對: -- 對";
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnRestart.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.btnRestart.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnRestart.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnRestart.Location = new System.Drawing.Point(221, 2);
+            this.btnRestart.Margin = new System.Windows.Forms.Padding(4);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.Size = new System.Drawing.Size(100, 31);
+            this.btnRestart.TabIndex = 2;
+            this.btnRestart.Text = "重新開始";
+            this.btnRestart.UseVisualStyleBackColor = false;
+            this.btnRestart.Click += new System.EventHandler(this.btnRestart_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // gamePanel
+            // 
+            this.gamePanel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.gamePanel.ColumnCount = 2;
+            this.gamePanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.gamePanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.gamePanel.Location = new System.Drawing.Point(16, 65);
+            this.gamePanel.MinimumSize = new System.Drawing.Size(500, 500);
+            this.gamePanel.Name = "gamePanel";
+            this.gamePanel.RowCount = 2;
+            this.gamePanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.gamePanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.gamePanel.Size = new System.Drawing.Size(510, 510);
+            this.gamePanel.TabIndex = 3;
+            // 
+            // lbls
+            // 
+            this.lbls.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lbls.AutoSize = true;
+            this.lbls.Location = new System.Drawing.Point(473, 46);
+            this.lbls.Name = "lbls";
+            this.lbls.Size = new System.Drawing.Size(29, 16);
+            this.lbls.TabIndex = 6;
+            this.lbls.Text = "--秒";
+            this.lbls.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // frmMemoryGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MintCream;
+            this.ClientSize = new System.Drawing.Size(543, 613);
+            this.Controls.Add(this.gamePanel);
+            this.Controls.Add(this.pnlButtom);
+            this.Controls.Add(this.pnlTop);
+            this.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.Name = "frmMemoryGame";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "記憶翻牌小遊戲";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMemoryGame_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.pnlTop.ResumeLayout(false);
+            this.pnlTop.PerformLayout();
+            this.pnlButtom.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlTop;
+        private System.Windows.Forms.Panel pnlButtom;
+        private System.Windows.Forms.Button btnLv3;
+        private System.Windows.Forms.Button btnLv2;
+        private System.Windows.Forms.Button btnLv1;
+        private System.Windows.Forms.ProgressBar timeBar;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Label lblRemaining;
+        private System.Windows.Forms.Button btnRestart;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.TableLayoutPanel gamePanel;
+        private System.Windows.Forms.Label lbls;
+    }
+}
+
---
+++ MemoryGame/frmMemoryGame.cs
@@ -0,0 +1,360 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text;
+
+namespace MemoryGame
+{
+    public partial class frmMemoryGame : Form
+    {
+        List<PictureBox> pics = new List<PictureBox>();
+        List<Image> images = new List<Image>();
+        PictureBox firstClick = null;
+        PictureBox secondClick = null;
+        bool lockClick = false;
+
+        int currentLevel = 1;      // 目前關卡：1=4x4, 2=6x6, 3=8x8, 4=10x10隱藏關
+        int remainingPairs = 0;    // 剩餘未配對的對數
+        int timeLeft = 0;          // 時間紀錄器（1~3關倒數，第4關正數）
+        int maxTime = 0;           // 本關總時間 (秒)
+
+        private Image BackImage = Properties.Resources.card_back;
+        [System.Runtime.InteropServices.DllImport("winmm.dll")]
+        private static extern long mciSendString(string strCommand, StringBuilder strReturn, int iReturnLength, IntPtr hwndCallback);
+
+        // 用來記錄每個獨立播放軌道的 ID
+        int soundIdCounter = 0;
+        // 用來記錄釋放後的實體音檔路徑
+        Dictionary<string, string> soundPathCache = new Dictionary<string, string>();
+
+        public frmMemoryGame()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            StartLevel(1);
+        }
+
+        /// <summary>
+        /// 核心方法：初始化並啟動指定關卡
+        /// </summary>
+        private void StartLevel(int level)
+        {
+            currentLevel = level;
+            firstClick = null;
+            secondClick = null;
+            lockClick = false;
+
+            // 目前關卡亮起，其餘變灰
+            btnLv1.Enabled = (level == 1);
+            btnLv2.Enabled = (level == 2);
+            btnLv3.Enabled = (level == 3);
+
+            btnLv1.BackColor = (level == 1) ? Color.DarkSeaGreen : Color.LightGray;
+            btnLv2.BackColor = (level == 2) ? Color.DarkSeaGreen : Color.LightGray;
+            btnLv3.BackColor = (level == 3) ? Color.DarkSeaGreen : Color.LightGray;
+
+            btnLv1.ForeColor = (level == 1) ? Color.White : Color.Black;
+            btnLv2.ForeColor = (level == 2) ? Color.White : Color.Black;
+            btnLv3.ForeColor = (level == 3) ? Color.White : Color.Black;
+
+            // 依關卡決定網格大小與限時
+            int rows = 4, cols = 4;
+            if (level == 1) { rows = 4; cols = 4; maxTime = 120; }       // 4x4 限時 120 秒
+            else if (level == 2) { rows = 6; cols = 6; maxTime = 300; }  // 6x6 限時 300 秒
+            else if (level == 3) { rows = 8; cols = 8; maxTime = 900; }  // 8x8 限時 900 秒
+            else if (level == 4) { rows = 10; cols = 10; maxTime = 0; }  // 10x10 正向計時
+
+            remainingPairs = (rows * cols) / 2;
+
+            // 設定計時模式與介面文字
+            if (level == 4)
+            {
+                timeLeft = 0; // 從 0 秒開始正向計時
+                timeBar.Maximum = 1500;
+                timeBar.Value = 0; // 隱藏關進度條全滿當裝飾
+                lblTime.Text = "⏱️ 已用時間：";
+                lblRemaining.Text = $"剩餘：{remainingPairs} 未配對";
+                this.Text = "終極隱藏關卡";
+
+            }
+            else
+            {
+                timeLeft = maxTime;
+                timeBar.Maximum = maxTime;
+                timeBar.Value = maxTime;
+                lblTime.Text = "⏱️ 剩餘時間：";
+                lblRemaining.Text = $"剩餘: {remainingPairs} 未配對";
+            }
+
+            // 清空舊的畫面與資料
+            gamePanel.Controls.Clear();
+            gamePanel.RowStyles.Clear();
+            gamePanel.ColumnStyles.Clear();
+            pics.Clear();
+            images.Clear();
+
+            // 網格
+            gamePanel.RowCount = rows;
+            gamePanel.ColumnCount = cols;
+            for (int i = 0; i < rows; i++) gamePanel.RowStyles.Add(new RowStyle(SizeType.Percent, 100f / rows));
+            for (int i = 0; i < cols; i++) gamePanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100f / cols));
+
+            // 隨機打亂 1~50 的數字，確保每場抽到的牌種類不重複
+            List<int> imgIndexes = Enumerable.Range(1, 50).ToList();
+            Random rand = new Random();
+            imgIndexes = imgIndexes.OrderBy(x => rand.Next()).ToList();
+
+            for (int i = 0; i < remainingPairs; i++)
+            {
+                // 根據隨機排序的數字，抓取 card_X
+                string resName = "card_" + imgIndexes[i];
+                Image img = (Image)Properties.Resources.ResourceManager.GetObject(resName);
+                if (img != null)
+                {
+                    images.Add(img);
+                }
+            }
+            images.AddRange(images); // 複製一份，達成兩兩配對
+
+            // 產生 PictureBox 加入網格
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < cols; c++)
+                {
+                    PictureBox p = new PictureBox();
+                    p.Dock = DockStyle.Fill;
+                    p.SizeMode = PictureBoxSizeMode.Zoom;
+                    p.Image = BackImage;
+                    p.Margin = new Padding(4);
+                    p.Click += pic_Click; // 綁定點擊事件
+
+                    pics.Add(p);
+                    gamePanel.Controls.Add(p, c, r);
+                }
+            }
+
+            // 洗牌
+            Random rd = new Random();
+            foreach (PictureBox p in pics)
+            {
+                int index = rd.Next(images.Count);
+                p.Tag = images[index];
+                images.RemoveAt(index);
+            }
+
+            // 啟動計時器
+            timer1.Interval = 1000;
+            timer1.Start();
+        }
+
+        private void pic_Click(object sender, EventArgs e)
+        {
+            PictureBox pic = sender as PictureBox;
+
+            if (lockClick) return;
+            if (pic.Image != BackImage) return; // 已翻開的不能再點
+
+            PlaySound("click_flip"); // 翻牌點擊音效
+
+            if (firstClick == null)
+            {
+                firstClick = pic;
+                pic.Image = (Image)pic.Tag;
+                return;
+            }
+
+            secondClick = pic;
+            pic.Image = (Image)pic.Tag;
+
+            // 比對兩張牌的 Tag
+            if (firstClick.Tag != secondClick.Tag)
+            {
+                lockClick = true;
+                PlaySound("match_fail"); // 配對失敗音效
+                timer2.Interval = 1000; // 延遲 1 秒翻回
+                timer2.Start();
+            }
+            else
+            {
+                // 配對成功！
+                firstClick.Click -= pic_Click;
+                secondClick.Click -= pic_Click;
+
+                remainingPairs--;
+                lblRemaining.Text = $"剩餘: {remainingPairs} 未配對";
+                PlaySound("match_success"); // 配對成功音效
+
+                firstClick = null;
+                secondClick = null;
+
+                CheckWin();
+            }
+        }
+
+        // 延遲一秒
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            timer2.Stop();
+
+            firstClick.Image = BackImage;
+            secondClick.Image = BackImage;
+
+            firstClick = null;
+            secondClick = null;
+            lockClick = false;
+        }
+
+        // 倒數與正數
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (currentLevel == 4)
+            {
+                timeLeft++;
+                lbls.Text = $"{timeLeft} 秒";
+                timeBar.Value = Math.Min(timeLeft, timeBar.Maximum);
+                return;
+            }
+
+            timeLeft--;
+            if (timeLeft >= 0)
+            {
+                timeBar.Value = timeLeft; // 倒數線縮短
+                lbls.Text = $"{timeLeft} 秒";
+            }
+
+            if (timeLeft <= 0)
+            {
+                timer1.Stop();
+                lockClick = true;
+                MessageBox.Show("⏰ 時間到！挑戰失敗，再試一次吧！", "遊戲結束");
+                StartLevel(currentLevel); // 失敗則重來本關
+            }
+        }
+
+        private void CheckWin()
+        {
+            if (remainingPairs == 0)
+            {
+                timer1.Stop();
+
+                if (currentLevel == 1 || currentLevel == 2)
+                {
+                    PlaySound("next_level");
+                    MessageBox.Show($"恭喜通過第 {currentLevel} 關！準備挑戰下一關！", "過關成功");
+
+                    currentLevel++;
+                    StartLevel(currentLevel); // 自動載入下一關
+                }
+                else if (currentLevel == 3)
+                {
+                    // 如果剩餘時間大於 200 秒，觸發隱藏關卡！
+                    if (timeLeft > 200)
+                    {
+                        PlaySound("next_level");
+
+                        DialogResult result = MessageBox.Show(
+                            $"驚人速通！你竟然還剩餘 {timeLeft} 秒！\n\n是否接受終極隱藏關卡 (10 x 10) 的挑戰？",
+                            "⚡ 隱藏挑戰觸發 ⚡",
+                            MessageBoxButtons.YesNo,
+                            MessageBoxIcon.Warning
+                        );
+
+                        if (result == DialogResult.Yes)
+                        {
+                            StartLevel(4); // 載入隱藏關卡
+                        }
+                        else
+                        {
+                            PlaySound("game_clear");
+                            MessageBox.Show("太強了！你成功完成挑戰，完美破關！", "遊戲結束");
+                        }
+                    }
+                    else
+                    {
+                        PlaySound("game_clear");
+                        MessageBox.Show("恭喜你完美通關！", "遊戲結束");
+                    }
+                }
+                else if (currentLevel == 4)
+                {
+                    PlaySound("game_clear");
+                    MessageBox.Show($"神之操作！你成功征服了 10 x 10 終極隱藏關卡！\n總共花費時間：{timeLeft} 秒！你就是記憶之王！", "神話誕生");
+                }
+            }
+        }
+
+        private void btnRestart_Click(object sender, EventArgs e)
+        {
+            this.Text = "記憶翻牌小遊戲";
+            StartLevel(1); // 重新從第一關開始挑戰
+        }
+
+        private void PlaySound(string soundName)
+        {
+            try
+            {
+                string filePath = "";
+
+                // 如果這個音檔還沒有被釋放到暫存區，先執行一次釋放
+                if (!soundPathCache.ContainsKey(soundName))
+                {
+                    byte[] soundBytes = null;
+                    System.IO.Stream str = (System.IO.Stream)Properties.Resources.ResourceManager.GetObject(soundName);
+                    if (str != null)
+                    {
+                        using (System.IO.MemoryStream ms = new System.IO.MemoryStream())
+                        {
+                            str.CopyTo(ms);
+                            soundBytes = ms.ToArray();
+                        }
+                    }
+
+                    if (soundBytes != null)
+                    {
+                        string tempPath = System.IO.Path.Combine(System.IO.Path.GetTempPath(), soundName + "_" + this.GetHashCode() + ".wav");
+                        System.IO.File.WriteAllBytes(tempPath, soundBytes);
+                        soundPathCache[soundName] = tempPath;
+                    }
+                }
+
+                if (soundPathCache.TryGetValue(soundName, out filePath))
+                {
+                    soundIdCounter++;
+                    string alias = $"{soundName}_{soundIdCounter}";
+                    mciSendString($"open \"{filePath}\" type waveaudio alias {alias}", null, 0, IntPtr.Zero);
+                    mciSendString($"play {alias} from 0", null, 0, IntPtr.Zero);
+                }
+            }
+            catch (Exception)
+            {
+                // 確保音效不崩潰
+            }
+        }
+
+        private void frmMemoryGame_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // 遊戲關閉時把暫存的音檔砍掉
+            foreach (var path in soundPathCache.Values)
+            {
+                try
+                {
+                    if (System.IO.File.Exists(path))
+                    {
+                        System.IO.File.Delete(path);
+                    }
+                }
+                catch { }
+            }
+        }
+    }
+}
